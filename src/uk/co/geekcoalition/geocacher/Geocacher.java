4c8376adfaedcb6b0ace99a8fd8392ebe1a1837e
Andrew Montgomery-Hurrell <darkliquid@darkliquid.co.uk>
2008-12-17 22:26:02 +0000

    First commit

---
+++ src/uk/co/geekcoalition/geocacher/Geocacher.java
@@ -0,0 +1,13 @@
+package uk.co.geekcoalition.geocacher;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Geocacher extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
