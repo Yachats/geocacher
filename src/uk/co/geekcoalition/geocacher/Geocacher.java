4c2ecb37d09a6ca53c77616b5b2a98e1c4c302f8
Andrew Montgomery-Hurrell <darkliquid@darkliquid.co.uk>
2008-12-18 19:56:40 +0000

    Basic mapview

--- src/uk/co/geekcoalition/geocacher/Geocacher.java
+++ src/uk/co/geekcoalition/geocacher/Geocacher.java
@@ -2,12 +2,30 @@ package uk.co.geekcoalition.geocacher;
 
 import android.app.Activity;
 import android.os.Bundle;
+import com.google.android.maps.MapActivity;
+import com.google.android.maps.MapController;
+import com.google.android.maps.MapView;
+import com.google.android.maps.GeoPoint; 
 
-public class Geocacher extends Activity {
+public class Geocacher extends MapActivity {
+	private MapView map;
+	
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        map = new MapView(this, "api_key_goes_here");
+        GeoPoint p = new GeoPoint((int) (40.689213 * 1000000), (int) (-74.044558 * 1000000));
+        MapController mc = map.getController();
+        mc.animateTo(p);
+        mc.setZoom(20);
+        setContentView(map);
+        map.setSatellite(true); 
+    }
+    
+    @Override
+    public boolean isRouteDisplayed() {
+    	return false;
     }
 }
